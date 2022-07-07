84530d23098462852c3d74034f3b86e7d0a3b398
Tayanne <tayh.snovaes@gmail.com>
2022-07-07 13:51:04 -0300

    refactor index.tsx

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { Avatar } from '../Avatar';
 import { Comment } from '../Comment';
 import styles from './index.module.css';
 
-export function Post({ author, publishedAt, content }) {
+interface Author {
+   name: string;
+   role: string;
+   avatarUrl: string;
+}
+
+interface Content {
+   type: 'paragraph' | 'link';
+   content: string;
+}
+
+interface PostProps {
+   author: Author;
+   publishedAt: Date;
+   content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
    const [comments, setComments] = useState(['Post top!!!']);
    const [newCommentText, setNewCommentText] = useState('');
 
@@ -20,23 +37,25 @@ export function Post({ author, publishedAt, content }) {
       addSuffix: true,
    });
 
-   const handleCreateNewComment = (event) => {
+   const handleCreateNewComment = (event: FormEvent) => {
       event.preventDefault();
 
       setComments([...comments, newCommentText]);
       setNewCommentText('');
    };
 
-   const handleNewCommentChange = (event) => {
+   const handleNewCommentChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
       event.target.setCustomValidity('');
       setNewCommentText(event.target.value);
    };
 
-   const handlenewCommentInvalid = (event) => {
+   const handlenewCommentInvalid = (
+      event: InvalidEvent<HTMLTextAreaElement>
+   ) => {
       event.target.setCustomValidity('Esse campo é obrigatório!');
    };
 
-   const deleteComment = (commentToDelete) => {
+   const deleteComment = (commentToDelete: string) => {
       const commentsWithoutDeletedOne = comments.filter(
          (comment) => comment !== commentToDelete
       );
